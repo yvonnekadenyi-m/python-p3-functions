8026ba0eace884a5d6713d66320f7516411deb8d
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-23 08:32:19 +0300

    added to functions.py

--- lib/functions.py
+++ lib/functions.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(): 
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
 
 def add(num1, num2):
     pass
